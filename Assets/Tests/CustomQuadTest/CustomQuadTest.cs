b14bda3057e62597fbb620511f85687219bdc3df
Carl Emil Carlsen (OSX) <public@cec.dk>
2019-01-24 15:15:18 +0100

    Added CustomQuadTest

---
+++ Assets/Tests/CustomQuadTest/CustomQuadTest.cs
@@ -0,0 +1,104 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomQuadTest : MonoBehaviour
+{
+	// Camera space: 	positions relative to the camera position and rotation.
+	// Clip space: 	positions relative to a cube that contains what the camera can see: (-1,-1,-1) to (1,1,1).
+	// Sensor space: 	positions relative to near clip plane (-1,-1,1) to (1,1,1)
+	// To go from camera space to clip space, multiply position by _camera.projectionMatrix.
+
+	// Sensor space points to be transformed.
+	// Order: lower-left, upper-let, upper-right, lower-right.
+	static readonly Vector3[] sensorSourcePoints = { new Vector3( -1, -1, -1), new Vector3( -1, 1, -1 ), new Vector3( 1, 1, -1 ), new Vector3( 1, -1, -1 ) };
+	
+	// Points set by the user in 2D at (near clip space).
+	Vector3[] _sensorUserPoints = new Vector3[4];
+	
+	// UI.
+	int _selectedCornerIndex = -1;
+	
+	// Result.
+	Matrix4x4 _homography;
+
+	Material _material;
+
+	Mesh _mesh;
+	
+	
+	void Awake()
+	{
+		// Start with user points at source points.
+		sensorSourcePoints.CopyTo( _sensorUserPoints, 0 );
+
+		// Create material.
+		_material = new Material( Shader.Find( "Hidden/HomographytCustomQuadTest" ) );
+
+		// Create mesh.
+		_mesh = new Mesh();
+		_mesh.vertices = new Vector3[] { new Vector3( -1, -1, 0 ), new Vector3( -1, 1, 0 ), new Vector3( 1, 1, 0 ), new Vector3( 1, -1, 0 ) };
+		_mesh.uv = new Vector2[] { new Vector2(0,0), new Vector2(0,1), new Vector2( 1, 1 ), new Vector2(1,0) };
+		_mesh.triangles = new int[] { 0, 1, 2, 2, 3, 0 };
+	}
+
+
+	void Update()
+	{
+		UpdateUserInteraction();
+	}
+
+
+	void OnRenderObject()
+	{
+		Camera cam = Camera.current;
+		if( cam.cameraType != CameraType.Game ) return;
+
+
+		// Transformed quad.
+		Homography.Find( _sensorUserPoints, ref _homography );
+		_material.SetMatrix( "_Matrix", _homography );
+		_material.SetPass( 0 );
+		Graphics.DrawMeshNow( _mesh, Matrix4x4.identity );
+
+		// Handles.
+		for( int i = 0; i < 4; i++ ){
+			//Debug.Log( _sensorUserPoints[i] );
+			Matrix4x4 mat = Matrix4x4.TRS( (Vector2) _sensorUserPoints[i], Quaternion.identity, new Vector3(1,cam.aspect,1) * 0.01f );
+			_material.SetMatrix( "_Matrix", mat );
+			_material.SetPass( 0 );
+			Graphics.DrawMeshNow( _mesh, Matrix4x4.identity );
+		}
+	}
+
+
+	void UpdateUserInteraction()
+	{
+		Camera cam = Camera.main;
+
+		// Get mouse position and transform it to clip space.
+		Vector2 mousePosition = Input.mousePosition;
+		mousePosition.y = cam.pixelHeight - mousePosition.y; // flip vertically
+		mousePosition.Scale( new Vector2( 2/(float) cam.pixelWidth, 2/(float) cam.pixelHeight ) );
+		mousePosition -= Vector2.one;
+
+		// Select (nearest corner) & deselect.
+		if( Input.GetMouseButtonDown( 0 ) ){
+			float closestDist = float.MaxValue;
+			for( int i = 0; i < 4; i++ ){
+				float dist = Vector2.Distance( _sensorUserPoints[i], mousePosition );
+				if( dist < closestDist ){
+					closestDist = dist;
+					_selectedCornerIndex = i;
+				}
+			}
+		} else if( Input.GetMouseButtonUp( 0 ) ) {
+			_selectedCornerIndex = -1;
+		}
+
+		// Update selected user point from mouse position.
+		if( _selectedCornerIndex != -1 ) {
+			_sensorUserPoints[_selectedCornerIndex].Set( mousePosition.x, mousePosition.y, -1 );
+		}
+	}
+}
---
+++ Assets/Tests/CustomQuadTest/Resources/HomographytCustomQuadTest.shader
@@ -0,0 +1,60 @@
+﻿Shader "Hidden/HomographytCustomQuadTest"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	
+	SubShader
+	{
+		CGINCLUDE
+		
+		#include "UnityCG.cginc"
+
+		struct ToVert
+		{
+			float4 vertex : POSITION;
+			float2 uv : TEXCOORD0;
+		};
+
+		struct ToFrag
+		{
+			float2 uv : TEXCOORD0;
+			float4 vertex : SV_POSITION;
+		};
+		
+		
+		float4x4 _Matrix;
+		
+		
+		ToFrag Vert( ToVert v )
+		{
+			ToFrag o;
+			o.vertex = v.vertex;
+			//o.vertex = UnityObjectToClipPos( v.vertex );
+			o.vertex = mul( _Matrix, o.vertex );
+			o.uv = v.uv;
+			return o;
+		}
+		
+		fixed4 Frag( ToFrag i ) : SV_Target
+		{
+			return fixed4( i.uv, 0, 1 );
+		}
+			
+		ENDCG
+		
+		
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+		
+		// Pass 0: Draw image.
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex Vert
+			#pragma fragment Frag
+			ENDCG
+		}
+	}
+}
